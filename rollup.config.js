356eb0276f77e197706e0db248f6039ef389709e
Tom Jeatt <tom.jeatt@darkbeam.com>
2021-05-28 15:27:32 +0100

    Typo in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript2";
+import typescript from "rollup-plugin-typescript2";
 
 const packageJson = require("./package.json");
 
