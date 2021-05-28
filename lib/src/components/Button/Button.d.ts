19dd6f72055b76cc0efb0b442e447ea734b3d8a1
Tom Jeatt <tom.jeatt@darkbeam.com>
2021-05-28 15:22:56 +0100

    Working rollup config

--- lib/src/components/Button/Button.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export interface Button {
-    label: string;
-}
-export declare const Button: React.FC<Button>;
--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,28 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
-import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript2";
 
-// this override is needed because Module format cjs does not support top-level await
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require("./package.json");
-
-const globals = {
-  ...packageJson.devDependencies,
-};
 
 export default {
   input: "src/index.ts",
   output: [
     {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
       file: packageJson.module,
-      format: "esm", // ES Modules
+      format: "esm",
+      sourcemap: true,
     },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: {
-        exclude: ["**/*.stories.*"],
-      },
-    }),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
   ],
-  external: Object.keys(globals),
 };
